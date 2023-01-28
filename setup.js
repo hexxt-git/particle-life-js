7ce770b515800a488e83277e522f95b7011c540a
9EED <zeghdns@gmail.com>
2023-01-28 16:27:02 +0100

    *

--- setup.js
+++ setup.js
@@ -3,7 +3,7 @@ let canvas = $('canvas')
 let c = canvas.getContext('2d')
 let width = container.clientWidth
 let height = container.clientHeight
-let fps = 50
+let fps = 100
 
 canvas.width = width
 canvas.height = height
@@ -13,16 +13,17 @@ c.strokeStyle = '#CCC'
 c.font = 'bold 30px monospace';
 
 let sim_settings = {
-    universal_repulsion: 1,
+    universal_repulsion: .6,
     global_multiplier: 6,
-    distance_multiplier: 1/50,
-    max_velocity: 100
+    distance_multiplier: 1/70,
+    max_velocity: 100,
+    friction: .8
 }
 
 let camera = {
-    x: -width / 2,
-    y: -height / 2,
-    z: .6,
+    x: - 100 - width / 2,
+    y: - 100 - height / 2,
+    z: .5,
     w: window.innerWidth,
     h: window.innerHeight
 }
@@ -52,23 +53,44 @@ window.addEventListener( 'mouseleave', ()=>{
 }) 
 window.addEventListener( 'keypress', (key)=>{
     if( key.key == 'r') location.reload()
+    if( key.key == '+') camera.z *= 1.1
+    if( key.key == '-') camera.z *= 0.9
 })
 
 class Grid{
     constructor(width, height, cell_size) {
-        this.width = Math.ceil(width/cell_size) * cell_size 
-        this.height = Math.ceil(height/cell_size) * cell_size
+        this.w = Math.ceil(width/cell_size)
+        this.h = Math.ceil(height/cell_size)
+        this.width = this.w * cell_size 
+        this.height = this.h * cell_size
         this.cell_size = cell_size
         this.grid = new Array(Math.ceil(this.height/this.cell_size)).fill(new Array(Math.ceil(this.width/this.cell_size)).fill([]))
     }
     update(particles){
-        this.grid = new Array(Math.ceil(this.height/this.cell_size)).fill('').map( x => new Array(Math.ceil(this.width/this.cell_size)).fill('').map(x => []))
+        this.grid = new Array(this.h).fill('').map( x => new Array(this.w).fill('').map(x => []))
 
         for(let p of particles){
             let x = Math.floor(p.x / this.cell_size)
             let y = Math.floor(p.y / this.cell_size)
-            this.grid[y][x].push(p)
+            this.get_cell(x, y).push(p)
         }
+    }
+    get_nearby(x, y, r){
+        let look_up_circle = getCircle(Math.round(x / this.cell_size), Math.round(y / this.cell_size), r)
+        let found = []
+        for(let i in look_up_circle){
+            let x = look_up_circle[i].x
+            let y = look_up_circle[i].y
+            if(x < 0) x += this.w
+            if(y < 0) y += this.h
+            if(x >= this.w) x -= this.w
+            if(y >= this.h) y -= this.h
+            found.push(this.get_cell(x, y))
+        }
+        return found
+    }
+    get_cell(x, y){
+        if (y >= 0 && y < this.h && x >= 0 && x < this.w) return this.grid[y][x]
     }
     render(){
         for(let y in this.grid){
@@ -116,23 +138,12 @@ class Particle{
         render_circle(camera, this.x, this.y, 5, types[this.type].color)
     }
     update(grid){
-        let look_up_circle = getCircle(Math.round(this.x / grid.cell_size), Math.round(this.y / grid.cell_size), 2)
-        for(let i in look_up_circle){
-            let x = look_up_circle[i].x % (grid.width / grid.cell_size)
-            let y = look_up_circle[i].y % (grid.height / grid.cell_size)
-            x = Math.abs(x)
-            y = Math.abs(y)
-            for(let p of grid.grid[y][x]){
-                if(p == this) continue
-                solver(this, p)
-            }
-            this.vx *= .9
-            this.vy *= .9
-            if (this.vx > sim_settings.max_velocity) this.vx = sim_settings.max_velocity
-            if (this.vx < -sim_settings.max_velocity) this.vx = -sim_settings.max_velocity
-            if (this.vy > sim_settings.max_velocity) this.vy = sim_settings.max_velocity
-            if (this.vy < -sim_settings.max_velocity) this.vy = -sim_settings.max_velocity
+        let found = grid.get_nearby(this.x, this.y, 2)
+        for(let p of found){
+            if(p == this) continue
+            solver(this, p)
         }
+
         this.x += this.vx
         this.y += this.vy
         if(this.x < 0) this.x = grid.width - 1
@@ -141,12 +152,7 @@ class Particle{
         if(this.y >= grid.height) this.y = 1
     }
 }
-function random_type(){
-    return ['red', 'green', 'blue'][rdm(2)]
-}
-function random_particle(){
-    return new Particle(rdm(1499,1), rdm(799, 1), random_type())
-}
+
 function solver(p1, p2){
     let relation = types[p1.type][p2.type]
     let distance = Math.sqrt((p1.x-p2.x)**2 + (p1.y-p2.y)**2) * sim_settings.distance_multiplier
@@ -158,8 +164,14 @@ function solver(p1, p2){
 
 }
 
+function random_type(){
+    return ['red', 'green', 'blue'][rdm(2)]
+}
+function random_particle(){
+    return new Particle(rdm(1699,1), rdm(1199, 1), random_type())
+}
 let grid = new Grid(1700, 1200, 80)
-let particles = new Array(1000).fill('').map(x => random_particle())
+let particles = new Array(500).fill('').map(x => random_particle())
 
 
 
